475959551ede64d2fdf2bd817e5895cf76aea07c
amir-dell <amir.h.kadivar@gmail.com>
2013-06-28 17:11:04 -0400

    redefine workflow directory structures

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,13 +6,8 @@ default['deploy-drupal']['admin_pass']            = 'admin'
 # vhost server name
 default['deploy-drupal']['site_name']             = 'cooked.drupal' 
 
-# absolute path to drupal SQL dump (can be .gz)
-default['deploy-drupal']['sql_load_file']         = '' 
-
-# absolute path to bash script to run after loading SQL dump
-default['deploy-drupal']['sql_post_load_script']  = '' 
-
-#should be consistent with  node['apache']['listen_ports']
+# apache vhost port should be consistent with 
+# node['apache']['listen_ports']
 default['deploy-drupal']['apache_port']           = "80" 
 
 # user owning drupal codebase files
@@ -23,14 +18,28 @@ default['deploy-drupal']['apache_group']          = 'www-data'
 # group owning drupal codebase files
 default['deploy-drupal']['dev_group']             = 'sudo'
 
-#required attribute to drupal folder containing index.php and settings.php
-default['deploy-drupal']['codebase_source_path']  = ''  
+# path to the root of an existing project to be 
+# loaded, the contents of this path will be 
+# copied to deploy_project_base/site_name/
+default['deploy-drupal']['source_project_path']   = ''
 
-# can be same as codebase_source_path
-default['deploy-drupal']['deploy_directory']      = "/var/shared/sites/#{node['deploy-drupal']['site_name']}/site" 
+# root of the Drupal site, relative to project_path
+# this must be such that project_path/drupal_site_path/ contains index.php
+default['deploy-drupal']['source_site_path']      = ''
 
-# path to Drupal files directory, relative to site root
-default['deploy-drupal']['files_path']            = 'sites/default/files/'
+# absolute path to deployment directory
+# the Drupal site root would be deploy_path/site_name/site
+default['deploy-drupal']['deploy_base_path']      = '/var/shared/sites'
+
+# path to Drupal "files" directory, relative to site root
+default['deploy-drupal']['site_files_path']       = 'sites/default/files'
+
+# path to drupal SQL dump (can be .gz), relative to source_project_path
+default['deploy-drupal']['sql_load_file']         = 'db/dump.sql.gz' 
+
+# absolute path to bash script to run after loading SQL dump
+default['deploy-drupal']['post_script_file']      = ''
+
 
 # MySQL username and password used by Drupal
 default['deploy-drupal']['mysql_user']            = 'drupal_db'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,60 +7,79 @@ include_recipe 'deploy-drupal::lamp_stack'
 include_recipe 'deploy-drupal::pear_dependencies'
 
 
-MYSQL_ROOT_PASS = node['mysql']['server_root_password']
-MYSQL_DRUPAL_USER = node['deploy-drupal']['mysql_user']
-MYSQL_DRUPAL_PASS = node['deploy-drupal']['mysql_pass']
-DRUPAL_DB_NAME = node['deploy-drupal']['db_name']
+DRUPAL_TRUSTEES     = node['deploy-drupal']['dev_group']
+
+SOURCE_PROJECT_DIR  = node['deploy-drupal']['source_project_path']
+SOURCE_SITE_DIR     = SOURCE_PROJECT_DIR + "/"
+                      node['deploy-drupal']['source_site_path']
+
+SOURCE_DB_FILE      = SOURCE_PROJECT_DIR + "/" +
+                      node['deploy-drupal']['sql_load_file']
+SOURCE_SCRIPT_FILE  = SOURCE_PROJECT_DIR + "/" +
+                      node['deploy-drupal']['post_script_file']
+
+
+DRUPAL_SITE_NAME    = node['deploy-drupal']['site_name']
+DEPLOY_PROJECT_DIR  = node['deploy-drupal']['deploy_base_path']+
+                      "/#{DRUPAL_SITE_NAME}"
+DEPLOY_SITE_DIR     = "#{DEPLOY_PROJECT_DIR}/site"
+DEPLOY_FILES_DIR    = DEPLOY_SITE_DIR + "/" +
+                      node['deploy-drupal']['site-files-path']
+DEPLOY_SQL_LOAD_FILE= DEPLOY_PROJECT_DIR + "/" +
+                      node['deploy-drupal']['sql_load_file']
+DEPLOY_SCRIPT_FILE  = DEPLOY_PROJECT_DIR + "/" +
+                      node['deploy-drupal']['post_script_file']
+
+APACHE_PORT         = node['deploy-drupal']['apache_port']
+APACHE_USER         = node['deploy-drupal']['apache_user']
+APACHE_GROUP        = node['deploy-drupal']['apache_group']
+
+DESTROY_EXISTING    = node['deploy-drupal']['destroy_existing']
+
+DB_ROOT_PASS        = node['mysql']['server_root_password']
+DRUPAL_DB_USER      = node['deploy-drupal']['mysql_user']
+DRUPAL_DB_PASS      = node['deploy-drupal']['mysql_pass']
+DRUPAL_DB_NAME      = node['deploy-drupal']['db_name']
+DRUPAL_ADMIN_PASS   = node['deploy-drupal']['admin_pass']
 
 # the format mysql -u <user> -p<password> ... causes errors when password is empty
 # Note that the mysql url with an empty password (password=''), as used in drush site-install, does not cause an error
+DB_DRUPAL_CONNECTION="mysql --user='#{DRUPAL_DB_USER}'\
+                            --host='localhost'\
+                            --password='#{DRUPAL_DB_PASS}'\
+                            --database='#{DRUPAL_DB_NAME}'"
+DB_ROOT_CONNECTION  ="mysql --user='root'\
+                            --host='localhost'\
+                            --password='#{DB_ROOT_PASS}'"
 
-MYSQL_DRUPAL_CONNECTION =  "mysql  --user='#{MYSQL_DRUPAL_USER}'\
-                                   --host='localhost'\
-                                   --password='#{MYSQL_DRUPAL_PASS}'\
-                                   --database='#{DRUPAL_DB_NAME}'"
+directory DEPLOY_PROJECT_DIR do
+  owner DRUPAL_TRUSTEES
+  group DRUPAL_TRUSTEES
+  recursive true 
+end
 
-MYSQL_ROOT_CONNECTION =    "mysql  --user='root'\
-                                   --host='localhost'\
-                                   --password='#{MYSQL_ROOT_PASS}'"
-
-SQL_LOAD_FILE = node['deploy-drupal']['sql_load_file']
-SQL_POST_LOAD_SCRIPT = node['deploy-drupal']['sql_post_load_script']
-
-DRUPAL_ADMIN_PASS = node['deploy-drupal']['admin_pass']
-DRUPAL_SITE_NAME = node['deploy-drupal']['site_name']
-DRUPAL_TRUSTEES = node['deploy-drupal']['dev_group']
-
-DRUPAL_SOURCE_PATH = node['deploy-drupal']['codebase_source_path']
-DRUPAL_DEPLOY_DIR = node['deploy-drupal']['deploy_directory']
-
-APACHE_PORT = node['deploy-drupal']['apache_port']
-APACHE_USER = node['deploy-drupal']['apache_user']
-APACHE_GROUP = node['deploy-drupal']['apache_group']
-
-DESTROY_EXISTING = node['deploy-drupal']['destroy_existing']
-
-directory DRUPAL_DEPLOY_DIR do
+directory DEPLOY_SITE_DIR do
   owner APACHE_USER
-  group APACHE_GROUP #specific to your usecase; perhaps should default to Vagrant
+  group DRUPAL_TRUESTEES 
   recursive true
 end
 
 execute "validate-drush-works" do
   command "drush status"
-  cwd DRUPAL_DEPLOY_DIR
+  cwd DEPLOY_SITE_DIR
 end
+# TODO decouple from Vagrant
 # destroy contents of drupal root folder if DESTROY_EXISTING is set
 # keeps a drush archive-dump in vagrant shared folder (/vagrant/ hardcoded)
 bash "destroy-existing-site" do
-  cwd DRUPAL_DEPLOY_DIR
+  cwd DEPLOY_PROJECT_DIR
   code <<-EOH
-    cd #{DRUPAL_SITE_NAME}
+    cd #{DEPLOY_SITE_DIR}
     drush archive-dump --tar-options="--exclude=.git" --destination=/vagrant/drupal_archive_dump.tar
     drush sql-query "DROP DATABASE #{DRUPAL_DB_NAME};"
-    rm -rf #{DRUPAL_DEPLOY_DIR}/*
+    rm -rf #{DEPLOY_PROJECT_DIR}/*
   EOH
-  only_if {DESTROY_EXISTING == "true"}
+  only_if { DESTROY_EXISTING == "true" }
 end
 
 # Copies drupal codebase from DRUPAL_SOURCE_PATH to DRUPAL_DEPLOY_DIR
@@ -68,35 +87,38 @@ bash "copy-drupal-site" do
   # see http://superuser.com/a/367303 for cp syntax discussion
   # assumes target directory already exists
   code <<-EOH
-    cp -Rf #{DRUPAL_SOURCE_PATH}/. '#{DRUPAL_DEPLOY_DIR}'
+    cp -Rf  #{SOURCE_PROJECT_DIR}/.  '#{DEPLOY_PROJECT_DIR}/'
   EOH
   # If identical, `creates "index.php"` will prevent resource execution.
   # This is great if you want to deploy directly to Vagrant shared folder
-  creates "#{DRUPAL_DEPLOY_DIR}/index.php"
+  creates "#{DEPLOY_SITE_DIR}/index.php"
   notifies :restart, "service[apache2]", :delayed
-  only_if "test -d '#{DRUPAL_SOURCE_PATH}' && test -f '#{DRUPAL_SOURCE_PATH}/index.php'"
+  only_if 
+    "test -d '#{SOURCE_SITE_DIR}' && \
+     test -f '#{SOURCE_SITE_DIR}/index.php'"
 end
 
 bash "download-drupal" do
   # download Drupal if there is no index.php in the source path
-  cwd "#{DRUPAL_DEPLOY_DIR}/.."
+  cwd "#{DEPLOY_PROJECT_DIR}/.."
 
   code <<-EOH
     drush dl drupal-7 --destination=. --drupal-project-rename=site -y
   EOH
-  creates "#{DRUPAL_DEPLOY_DIR}/index.php"
+  creates "#{DEPLOY_SITE_DIR}/index.php"
   notifies :restart, "service[apache2]", :delayed
   # not if there is already stuff copied over to deployment directory
-  not_if "test -d '#{DRUPAL_DEPLOY_DIR}' && test -f '#{DRUPAL_DEPLOY_DIR}/index.php'"
+  not_if 
+    "test -d '#{DEPLOY_SITE_DIR}' && \
+     test -f '#{DEPLOY_SITE_DIR}/index.php'"
 end
-
 
 web_app DRUPAL_SITE_NAME do
   template "web_app.conf.erb"
   port APACHE_PORT
   server_name DRUPAL_SITE_NAME
   server_aliases [DRUPAL_SITE_NAME]
-  docroot DRUPAL_DEPLOY_DIR
+  docroot DEPLOY_SITE_DIR
   notifies :restart, "service[apache2]", :delayed
 end
 
@@ -108,60 +130,69 @@ apache_site "000-default" do
 end
 
 
-#TODO Secure MySql database (tighten privileges and remove anonymous and @% users)
+#TODO Secure MySql database (tighten privileges and 
+# remove anonymous and @ % users)
 bash "add-mysql-user" do
-  # is idempotent since "create user" and "grant all" are idempotent
   code <<-EOH
-    #{MYSQL_ROOT_CONNECTION} -e "CREATE DATABASE IF NOT EXISTS #{DRUPAL_DB_NAME};"
-    #{MYSQL_ROOT_CONNECTION} -e "GRANT ALL ON #{DRUPAL_DB_NAME}.* TO '#{MYSQL_DRUPAL_USER}'@'localhost' IDENTIFIED BY '#{MYSQL_DRUPAL_PASS}'; FLUSH PRIVILEGES;"
-    #{MYSQL_DRUPAL_CONNECTION} -e "SHOW TABLES;"
+    #{DB_ROOT_CONNECTION} -e "CREATE DATABASE IF NOT EXISTS #{DRUPAL_DB_NAME};"
+    #{DB_ROOT_CONNECTION} -e "GRANT ALL ON #{DRUPAL_DB_NAME}.* TO
+    '#{DRUPAL_DB_USER}'@'localhost' IDENTIFIED BY '#{DRUPAL_DB_PASS}'; FLUSH PRIVILEGES;"
+    #{DB_DRUPAL_CONNECTION} -e "SHOW TABLES;"
   EOH
 end
 
 # load the drupal database from specified local SQL file
 execute "load-drupal-db-from-sql" do
-  cwd DRUPAL_DEPLOY_DIR 
+  cwd DEPLOY_SITE_DIR
+  
   #TODO: not robust to errors connecting to DB
   mysql_empty_check_cmd = "drush sql-query 'show tables;' | wc -l | xargs test 0 -eq"
 
   # SQL_LOAD_FILE might be nil, must be quoted
-  only_if "test -f '#{SQL_LOAD_FILE}'  && #{mysql_empty_check_cmd}", :cwd => DRUPAL_DEPLOY_DIR
+  only_if 
+    "test -f '#{DEPLOY_SQL_LOAD_FILE}' && #{mysql_empty_check_cmd}"
+  
   # Using zless instead of cat/zcat to optionally support gzipped files 
   # "`drush sql-connect`" because "drush sqlc" returns 0 even on connection failure
-  command "zless '#{SQL_LOAD_FILE}' | `drush sql-connect`"
+  command "zless '#{DEPLOY_SQL_LOAD_FILE}' | `drush sql-connect`"
   notifies :run, "execute[drush cache-clear]"
 end
 
 execute "drush-site-install" do
-  cwd DRUPAL_DEPLOY_DIR
+  cwd DEPLOY_SITE_DIR
+  
   # fixes sendmail error https://drupal.org/node/1826652#comment-6706102
-  command "php -d sendmail_path=/bin/true /usr/share/php/drush/drush.php site-install standard -y \
-               --account-name=admin --account-pass=#{DRUPAL_ADMIN_PASS} \
-               --db-url=mysql://#{MYSQL_DRUPAL_USER}:'#{MYSQL_DRUPAL_PASS}'@localhost/#{DRUPAL_DB_NAME} \
-               --site-name='#{DRUPAL_SITE_NAME}' --clean-url=0"
+  command "php -d sendmail_path=/bin/true /usr/share/php/drush/drush.php \
+                site-install standard -y \
+                --account-name=admin --account-pass=#{DRUPAL_ADMIN_PASS} \
+                --db-url=mysql://#{DRUPAL_DB_USER}:'#{DRUPAL_DB_PASS}'@localhost/#{DRUPAL_DB_NAME} \
+                --site-name='#{DRUPAL_SITE_NAME}' 
+                --clean-url=0"
+  
   # requires drush 6
-  only_if "drush status --fields=db-status | grep Connected | wc -l | xargs test 0 -eq", :cwd => DRUPAL_DEPLOY_DIR
-  #notifies :run, "execute[drush-check-for-updates]"
+  only_if 
+    "drush status --fields=db-status | grep Connected | wc -l | xargs test 0 -eq",
+    :cwd => DEPLOY_SITE_DIR
   notifies :run, "execute[drush-suppress-http-status-error]"
 end
 
 execute "drush-suppress-http-status-error" do
-  cwd DRUPAL_DEPLOY_DIR
+  cwd DEPLOY_SITE_DIR
   command "drush vset -y drupal_http_request_fails FALSE"
   action :nothing
 end
 
 # drush cache clear
 execute "drush cache-clear" do
-  cwd DRUPAL_DEPLOY_DIR 
+  cwd DEPLOY_SITE_DIR 
   action :nothing
 end
 
 # run customized sql-post-load-script, if requested
 execute "customized-sql-post-load-script" do
-  command "bash '#{SQL_POST_LOAD_SCRIPT}'"
-  cwd DRUPAL_DEPLOY_DIR 
-  only_if "test -f '#{SQL_POST_LOAD_SCRIPT}'"
+  command "bash '#{DEPLOY_SCRIPT_FILE}'"
+  cwd DEPLOY_SITE_DIR
+  only_if "test -f '#{DEPLOY_SCRIPT_FILE}'"
   action :nothing
   subscribes :run, "execute[load-drupal-db-from-sql]"
 end
@@ -177,14 +208,14 @@ template "/usr/local/bin/drupal-perm.sh" do
   owner "root"
   group "root"
   variables({
-    :files_path => node['deploy-drupal']['files_path'],
+    :files_path => DEPLOY_FILES_PATH, 
     :user  => APACHE_USER,
     :group => DRUPAL_TRUSTEES 
   })
 end
 
 execute "fix-drupal-permissions" do
-  cwd DRUPAL_DEPLOY_DIR
-  Chef::Log.info(DRUPAL_DEPLOY_DIR)
+  cwd DEPLOY_SITE_DIR
+  Chef::Log.info(DEPLOY_SITE_DIR)
   command "bash drupal-perm.sh"
 end
