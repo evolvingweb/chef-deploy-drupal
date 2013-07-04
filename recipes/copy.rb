87a00e665d8f07cdb8dc9dc5593686f76dc23a7e
amir-dell <amir.h.kadivar@gmail.com>
2013-07-04 13:26:08 -0400

    fix minor error in copy recipe

--- recipes/copy.rb
+++ recipes/copy.rb
@@ -2,6 +2,16 @@
 ## Recipe:: copy
 ## copies the entire copy_project_from directory to deployment root
 
+# assemble all necessary query strings and paths
+SOURCE_SITE_DIR     = node['deploy-drupal']['copy_project_from'] + "/" +
+                      node['deploy-drupal']['site_path']
+
+DEPLOY_PROJECT_DIR  = node['deploy-drupal']['deploy_base_path']+
+                      "/#{node['deploy-drupal']['site_name']}"
+
+DEPLOY_SITE_DIR     = DEPLOY_PROJECT_DIR + "/" + node['deploy-drupal']['site_path']
+
+
 bash "copy-drupal-site" do 
   # see http://superuser.com/a/367303 for cp syntax discussion
   # assumes target directory already exists
