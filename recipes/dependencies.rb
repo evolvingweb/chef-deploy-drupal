e637bda6dc3f6438284e62c6fe71e800a64c15af
= <amir.h.kadivar@gmail.com>
2013-08-02 13:37:21 -0400

    use php_pear LWRP instead of installing package and configure APC shm_size to 128M

--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -16,12 +16,12 @@ end
 
 pkgs = value_for_platform(
   [ 'centos', 'redhat', 'fedora' ] => { #TODO needs testing
-    'default' => %w{ pcre-devel php-mcrypt php-pecl-apc }
+    'default' => %w{ pcre-devel php-mcrypt }
   },
   [ 'debian', 'ubuntu' ] => {
-    'default' => %w{ libpcre3-dev php5-mcrypt php-apc }
+    'default' => %w{ libpcre3-dev php5-mcrypt }
   },
-  'default' => %w{ libpcre3-dev php5-mcrypt php-apc }
+  'default' => %w{ libpcre3-dev php5-mcrypt}
 )
 
 # install all packages
@@ -29,4 +29,9 @@ pkgs.each {|pkg| package ( pkg ) { action :install } }
 
 # Install uploadprogress for better feedback during Drupal file uploads.
 # php_pear LWRP is installed as part of the PHP cookbook
+
 php_pear ('uploadprogress') { action :install }
+php_pear "APC" do
+  action :install
+  directives( :shm_size => "128M" )
+end
