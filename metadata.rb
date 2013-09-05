bd3fb7f23153b1f046350884bcecea52f14278a3
Amir Kadivar <amir.h.kadivar@gmail.com>
2013-09-05 12:24:38 -0400

    update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "deploy-drupal"
-maintainer       "Alex Dergachev"
-maintainer_email "alex@evolvingweb.ca"
+maintainer       "Amir Kadivar"
+maintainer_email "amir@evolvingweb.ca"
 license          "Apache 2.0"
 description      "Installs/Configures/Bootsraps Drupal"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
