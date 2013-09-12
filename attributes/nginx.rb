f98ea22626789e97c9c64a3fb892ac410dd05838
Amir Kadivar <amir.h.kadivar@gmail.com>
2013-09-12 11:40:56 -0400

    cleanup nginx recipe, only allow custom_file or fallback on simple default

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -1,35 +1,5 @@
 ## Cookbook Name:: deploy-drupal
 ## Attribute:: default
 
-default['deploy-drupal']['nginx']['port'] = "80"
-default['deploy-drupal']['nginx']['log_format'] =
-  '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent ' +
-  '"$http_referer" "$http_user_agent" '+
-  '"$http_x_forwarded_for" "$http_host" "$proxy_add_x_forwarded_for"'
-
-# list of pcre patterns to deny request if
-# any pattern matches the requested file extenstion
-default['deploy-drupal']['nginx']['extension_block_list'] =
-  [ 'engine','inc','info','install', 'module', 'profile',
-    'po','sh', '.*sql','theme','tpl(\.php)?','xtmpl' ]
-
-# list of pcre patterns to deny request if
-# any pattern matches the entire request location
-default['deploy-drupal']['nginx']['location_block_list'] = 
-  [ 'code-style\.pl','Entries.*', 'Repository','Root','Tag', 'Template' ]
-
-# list of pcre patterns to deny request if
-# any pattern matches any part of request location
-default['deploy-drupal']['nginx']['keyword_block_list'] = ['boost-crawler']
-
-# list of pcre patterns to serve files if
-# any pattern matches requested file extension
-# ( will be matched against [pattern](\.gz)? )
-default['deploy-drupal']['nginx']['static_content'] =
-  [ 'jpg','jpeg','gif','png','bmp','ico',
-    'pdf','ps','djvu','doc','docx','rtf', 'xls','htm',
-    'flv','mp3','wmv','wma','wav','ogg',
-    'mpg','mpeg','mpg4','mp4','avi',
-    'zip','bz2','tar','tgz','rar']
-
+default['deploy-drupal']['nginx']['port'] = '80'
 default['deploy-drupal']['nginx']['custom_site_file'] = ''
--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -7,30 +7,20 @@
 include_recipe 'nginx::default'
 conf_file = node['nginx']['dir'] + "/sites-available/" + 
             node['deploy-drupal']['project_name']
-# the following strings must be assembled using single quotes
-# as they will be used as pcre regular expressions for nginx
-ext_list = '\.(' + node['deploy-drupal']['nginx']['extension_block_list'].join('|') + ')$'
-location_list = '^(' +  node['deploy-drupal']['nginx']['location_block_list'].join('|') + ')$'
-keyword_list = '(' + node['deploy-drupal']['nginx']['keyword_block_list'].join('|') + ')'
-static_list = '\.(' + node['deploy-drupal']['nginx']['static_content'].join('|') + ')(\.gz)?$'
+
 # custom blocks file might be relative to project root
 custom_file = node['deploy-drupal']['nginx']['custom_site_file']
 if ( custom_file[0] != '/' && !custom_file.empty? )
   custom_file = "#{node['deploy-drupal']['project_root']}/#{custom_file}"
 end
+
 # load the nginx site template
 template conf_file do
   source "nginx_site.conf.erb"
   mode 0644
   owner "root"
   group "root"
-  variables({
-    :ext_list => ext_list,
-    :location_list => location_list,
-    :keyword_list => keyword_list,
-    :static_list => static_list,
-    :custom_file => custom_file
-  })
+  variables ({ 'custom_file' => custom_file })
   notifies :reload, "service[nginx]"
 end
 
